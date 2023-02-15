bbafc2391eb4756362d60c415e5186d545c29307
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-15 15:06:13 +0200

    task1/2

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function sum (arr){
    }
 }
 console.log(sum(arr));
+
+// task 1/2
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function sum1 (arr1){
+   let element1 = arr1.pop();
+   return arr1.length === 0 ? 
+   element1 
+   : element1 += sum1(arr1);
+   }
+console.log(sum1(arr1));
